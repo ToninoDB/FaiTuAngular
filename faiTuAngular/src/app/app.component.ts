39410b6c7d23b9e93686e354b02206456e84720c
Antonio Russomando <tonyrussomando@gmail.com>
2025-03-20 15:12:32 +0100

    update

--- faiTuAngular/src/app/app.component.ts
+++ faiTuAngular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactsListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'faiTuAngular';
--- faiTuAngular/src/app/contacts-list/contacts-list.component.ts
+++ faiTuAngular/src/app/contacts-list/contacts-list.component.ts
@@ -1,23 +1,45 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/core';
+
+interface Contatto {
+  nome: string;
+  numero: string;
+  email?: string;
+}
 
 @Component({
   selector: 'app-contacts-list',
-  imports: [],
+  imports: [NgIf, NgFor, FormsModule],
+  standalone: true,
   templateUrl: './contacts-list.component.html',
   styleUrl: './contacts-list.component.css',
 })
 export class ContactsListComponent {
-  contatti: Contatto[] = [];
-}
+  contatti: Contatto[] = [
+    { nome: 'Luigi', numero: '3495459837', email: '' },
+    { nome: 'Raimondo', numero: '34954534837', email: 'rai@example.com' },
+  ];
 
-class Contatto {
-  nome: string;
-  numero: string;
-  email?: string;
+  newContatto: Contatto = {
+    nome: '',
+    numero: '',
+    email: '',
+  };
 
-  constructor(nome: string, numero: string, email: string) {
-    this.nome = nome;
-    this.numero = numero;
-    this.email = email;
+  addContatto(): void {
+    if (this.newContatto.nome && this.newContatto.numero) {
+      this.contatti.push({
+        nome: this.newContatto.nome,
+        numero: this.newContatto.numero,
+        email: this.newContatto.email,
+      });
+
+      this.newContatto = { nome: '', numero: '', email: '' };
+    }
+  }
+
+  removeContatto(index: number): void {
+    this.contatti.splice(index, 1);
   }
 }
